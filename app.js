9385e089685c4ee24f4c8b84a0b30753b414a000
medelka <medelka86@gmail.com>
2024-05-25 22:32:47 +0200

    Get data from database - Install ejs from

--- app.js
+++ app.js
@@ -4,17 +4,25 @@ const port = 3000;
 const mongoose = require("mongoose");
 
 app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 
 const MyData = require("./models/myDataSchema");
 
 app.get("/", (req, res) => {
-  res.sendFile("./views/home.html", { root: __dirname });
+  MyData.find()
+    .then((result) => {
+      console.log(result);
+      res.render("home", { title: "Home page", posts: result });
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+  // res.render("home", { title: "Home page" }); // home est le fichier home.ejs . on fait comme si on etait dans le dossier views. donc on va directement a home.
 });
 
 app.get("/index.html", (req, res) => {
   res.send("<h1>data sent correctly</h1> ");
 });
-
 
 mongoose
   .connect(
@@ -29,21 +37,21 @@ mongoose
     console.error("Failed to connect to MongoDB", err);
   });
 
+app.post("/", (req, res) => {
+  // console.log(req.body)
+  // const myData = new MyData (req.body);
+  const myData = new MyData({
+    userName: req.body.userName,
+    userEmail: req.body.userEmail,
+    userPassword: req.body.userPassword,
+  });
 
-  app.post("/", (req, res) => {
-    // console.log(req.body)
-    // const myData = new MyData (req.body);
-    const myData = new MyData({
-      userName: req.body.userName,
-      userEmail: req.body.userEmail,
-      userPassword: req.body.userPassword,
-    });
-
-    myData.save()
+  myData
+    .save()
     .then(() => {
       res.redirect("/index.html");
     })
     .catch((err) => {
       console.error(err);
-    })
-      });
+    });
+});
